66ad4baf7d70feb3174923073dc8244dad33abe6
Zibgame <fanizicy@gmail.com>
2025-11-17 10:51:04 +0100

    Add .h

---
+++ include/minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 10:49:12 by zcadinot          #+#    #+#             */
+/*   Updated: 2025/11/17 10:50:01 by zcadinot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# define BIT_SIZE 8
+
+#endif
